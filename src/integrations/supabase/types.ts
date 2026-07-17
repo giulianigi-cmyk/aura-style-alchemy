2b5b9d99a592b495ce48b34565200837b56afb7a
giulianigi-cmyk <giulia.nigi@gmail.com>
2026-07-17 08:42:29 +0200

    Update types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -168,6 +168,7 @@ export type Database = {
           currency: string | null
           id: string
           image_url: string
+          material: string[]
           occasion: string | null
           price: number | null
           season: string | null
@@ -185,6 +186,7 @@ export type Database = {
           currency?: string | null
           id?: string
           image_url: string
+          material?: string[]
           occasion?: string | null
           price?: number | null
           season?: string | null
@@ -202,6 +204,7 @@ export type Database = {
           currency?: string | null
           id?: string
           image_url?: string
+          material?: string[]
           occasion?: string | null
           price?: number | null
           season?: string | null
@@ -232,7 +235,7 @@ type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
 
 type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
 
-export type Tables<
+export type Tables
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof DatabaseWithoutInternals },
@@ -261,7 +264,7 @@ export type Tables<
       : never
     : never
 
-export type TablesInsert<
+export type TablesInsert
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
@@ -286,7 +289,7 @@ export type TablesInsert<
       : never
     : never
 
-export type TablesUpdate<
+export type TablesUpdate
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
@@ -311,7 +314,7 @@ export type TablesUpdate<
       : never
     : never
 
-export type Enums<
+export type Enums
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
     | { schema: keyof DatabaseWithoutInternals },
@@ -328,7 +331,7 @@ export type Enums<
     ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
     : never
 
-export type CompositeTypes<
+export type CompositeTypes
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof DatabaseWithoutInternals },
@@ -345,6 +348,11 @@ export type CompositeTypes<
     ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
     : never
 
+export const Constants = {
+  public: {
+    Enums: {},
+  },
+} as const
 export const Constants = {
   public: {
     Enums: {},
